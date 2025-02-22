5e50dad0897665fc2eb3f1230867b5c2aa1494f2
DaianeFernandes1 <daianefernandes@unitins.br>
2025-02-22 18:10:50 -0300

    aula02

---
+++ script.js
@@ -0,0 +1,26 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    
+    const btn = document.getElementById("startButton");
+    btn.addEventListener("click", function () {
+        alert("Você será redirecionado para iniciar seu treino!");
+    });
+
+    
+    document.querySelectorAll("nav ul li a").forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+            
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop - 50,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+
+});
